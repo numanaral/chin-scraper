acac9ce399685a82e58dd77fa5536334544ab988
numanaral <ahmet.aral@ucalgary.ca>
2020-10-26 05:35:38 -0600

    misc

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,7 +3,7 @@ import PrettyInput from 'components/PrettyInput';
 
 const SearchForm = props => (
 	<form onSubmit={e => e.preventDefault()}>
-		<PrettyInput label="Search for Chinese Character(s)" {...props} />
+		<PrettyInput label="Search for Hanzi..." {...props} />
 		<br />
 		<small> <b>NOTE: </b>Only Hanzi (Chinese chars) will be recognized.</small>
 	</form>
