195f99312645cddfd0cd43eb1e4011350ce4c152
numanaral <ahmet.aral@ucalgary.ca>
2020-10-26 05:08:18 -0600

    add pretty links and update source

--- src/components/Source.js
+++ src/components/Source.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import PrettyLink from 'components/PrettyLink';
+
+const Container = styled.div`
+	margin-top: 10px;
+	right: 10px;
+	font-size: 12px;
+	@media (min-width: 769px) {
+		margin-top: 0;
+		top: 10px;
+		position: absolute;
+	}
+`;
+
 const Source = () => (
-	<div
-		style={{
-			position: 'absolute',
-			top: '10px',
-			right: '10px',
-			fontSize: '12px',
-		}}
-	>
+	<Container>
 		<span> Scraped from: </span>
-		<a href="https://www.hanzi5.com/">Hanzi 5</a>&nbsp;&&nbsp;<a href="https://translate.google.com/">Google Translate</a>
-		<br/>
-		Favicon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-		<br/>
+		<PrettyLink href="https://www.hanzi5.com/">Hanzi 5</PrettyLink>
+		&nbsp;&&nbsp;
+		<PrettyLink href="https://translate.google.com/">
+			Google Translate
+		</PrettyLink>
+		<br />
+		Favicon made by&nbsp;
+		<PrettyLink
+			href="https://www.flaticon.com/authors/freepik"
+			title="Freepik"
+		>
+			Freepik
+		</PrettyLink>
+		&nbsp;from&nbsp;
+		<PrettyLink href="https://www.flaticon.com/" title="Flaticon">
+			www.flaticon.com
+		</PrettyLink>
+		<br />
 		<span> App Created By: </span>
-		<a href="https://numanaral.tech/">Numan Aral</a>
-	</div>
+		<PrettyLink href="https://numanaral.tech/">Numan Aral</PrettyLink>
+		<br />
+		<br />
+	</Container>
 );
 
 export default Source;
