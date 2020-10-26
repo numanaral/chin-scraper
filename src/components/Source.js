d5fd05931ac9a11e92467202ca7f9e2f61c9833f
numanaral <ahmet.aral@ucalgary.ca>
2020-10-26 00:05:39 -0600

    add analytics and update source

--- src/components/Source.js
+++ src/components/Source.js
@@ -5,11 +5,16 @@ const Source = () => (
 			position: 'absolute',
 			top: '10px',
 			right: '10px',
-			fontSize: '10px',
+			fontSize: '12px',
 		}}
 	>
 		<span> Scraped from: </span>
-		<a href="https://www.hanzi5.com/">Hanzi 5</a>
+		<a href="https://www.hanzi5.com/">Hanzi 5</a>&nbsp;&&nbsp;<a href="https://translate.google.com/">Google Translate</a>
+		<br/>
+		Favicon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+		<br/>
+		<span> App Created By: </span>
+		<a href="https://numanaral.tech/">Numan Aral</a>
 	</div>
 );
 
