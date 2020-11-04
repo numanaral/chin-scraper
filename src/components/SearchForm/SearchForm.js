cc9dc3444ef747dcf564f6087194c0f8a11b1759
numanaral <ahmet.aral@ucalgary.ca>
2020-11-04 13:04:18 -0700

    organize: restructure folders

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,7 +5,9 @@ const SearchForm = props => (
 	<form onSubmit={e => e.preventDefault()}>
 		<PrettyInput label="Search for Hanzi..." {...props} />
 		<br />
-		<small> <b>NOTE: </b>Only Hanzi (Chinese chars) will be recognized.</small>
+		<small>
+			<b>NOTE: </b>Only Hanzi (Chinese chars) will be recognized.
+		</small>
 	</form>
 );
 
