1e100e6024baa15bd5395556ca5f64d7ac97b8df
numanaral <ahmet.aral@ucalgary.ca>
2020-11-22 21:14:47 -0700

    <TooltipButton />

---
+++ src/components/TooltipButton/types.js
@@ -0,0 +1,52 @@
+import { bool, func, oneOf, string } from 'prop-types';
+
+import { icon, color, buttonVariant } from 'components/types';
+
+const buttonTypes = oneOf(['submit', 'reset', 'button']);
+
+/** PropTypes */
+const propTypes = {
+	// ========== Tooltip props ==========
+	/**
+	 * - Tooltip to display
+	 */
+	tooltip: string.isRequired,
+	interactive: bool,
+	// ========== Button props ==========
+	onClick: func,
+	/**
+	 * - Background color variant
+	 * - 'default' | 'primary' | 'secondary' |
+	 * 'error' | 'warning' | 'info' | 'success'
+	 */
+	bg: color,
+	icon,
+	disabled: bool,
+	type: buttonTypes,
+	preventDefaultEvent: bool,
+	displayTooltipOnClickForMobile: bool,
+	// ========== TextButton props ==========
+	/**
+	 * - Text to display in a text button
+	 * - If filled, generates a TextTooltipButton
+	 */
+	text: string,
+	variant: buttonVariant,
+};
+const defaultProps = {
+	// Tooltip props
+	interactive: false,
+	// Button props
+	onClick: null,
+	bg: 'default',
+	icon: null,
+	disabled: false,
+	type: 'button',
+	preventDefaultEvent: false,
+	displayTooltipOnClickForMobile: true,
+	// TextButton props
+	text: null,
+	variant: 'contained',
+};
+
+export { buttonTypes, propTypes, defaultProps };
