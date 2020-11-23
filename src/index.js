dc44d728dbf1c0363a5e18396a1f5e57faa3da88
numanaral <ahmet.aral@ucalgary.ca>
2020-11-23 04:11:04 -0700

    setup serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'containers/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
@@ -10,3 +11,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	rootElement
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
