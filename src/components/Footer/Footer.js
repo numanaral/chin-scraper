800d0828e20dc8333e495b0b5212e397524f3037
numanaral <ahmet.aral@ucalgary.ca>
2020-11-22 23:11:43 -0700

    <Footer />

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Grid, useMediaQuery } from '@material-ui/core';
+import styled from 'styled-components';
+import TypographyWrapper from 'components/References/TypographyWrapper';
+import LinkWrapper from 'components/References/LinkWrapper';
+import AnimatedHeartIcon from './AnimatedHeartIcon';
+
+const ColoredBox = styled(Grid)`
+	${({ theme }) => `
+		background-color: ${theme.palette.background.paper};
+	`}
+	padding: 3px 20px;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	z-index: 5;
+`;
+
+const Footer = () => {
+	const smAndDown = useMediaQuery(theme => theme.breakpoints.down('sm'));
+	return (
+		!smAndDown && (
+			<ColoredBox container item alignItems="center" justify="flex-end">
+				<TypographyWrapper>Created with</TypographyWrapper>
+				<AnimatedHeartIcon />
+				<TypographyWrapper>by:&nbsp;</TypographyWrapper>
+				<TypographyWrapper>
+					<LinkWrapper href="https://numanaral.tech/">
+						Numan Aral
+					</LinkWrapper>
+				</TypographyWrapper>
+			</ColoredBox>
+		)
+	);
+};
+
+export default Footer;
