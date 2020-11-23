3917a703220bb15fde76f5f709b9ee867842c210
numanaral <ahmet.aral@ucalgary.ca>
2020-11-23 02:19:59 -0700

    wrap app with all the providers & bg

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import 'normalize.css';
+import smoothscroll from 'smoothscroll-polyfill';
+import { FirebaseAppProvider, SuspenseWithPerf } from 'reactfire';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import CenteredContainer from 'components/CenteredContainer';
-import Source from 'components/Source';
-import Scraper from 'containers/Scraper';
+import { firebaseConfig } from 'store/firebase';
+import ThemeProvider from 'providers/ThemeProvider';
+import NavigationBar from 'components/NavigationBar';
+import Footer from 'components/Footer';
+import Routes from 'routes';
+import LoadingIndicator from 'components/LoadingIndicator';
+import Background from './Background';
 
-const App = () => {
-	return (
-		<CenteredContainer>
-			<Source />
-			<Scraper />
-		</CenteredContainer>
-	);
-};
+// Smooth scroll polyfill
+smoothscroll.polyfill();
+
+const App = () => (
+	<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+		<Router>
+			<ThemeProvider>
+				{themeProps => (
+					<Background>
+						<NavigationBar />
+						<SuspenseWithPerf
+							fallback={<LoadingIndicator />}
+							traceId="load-scraper-status"
+						>
+							<Routes themeProps={themeProps} />
+						</SuspenseWithPerf>
+						<Footer />
+					</Background>
+				)}
+			</ThemeProvider>
+		</Router>
+	</FirebaseAppProvider>
+);
 
 export default App;
