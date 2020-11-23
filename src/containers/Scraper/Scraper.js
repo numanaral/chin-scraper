c811dbc6f3e241ecd83464ab4653cc22fdeebac7
numanaral <ahmet.aral@ucalgary.ca>
2020-11-22 23:50:59 -0700

    Abstract Scraper utils, remove init speak()

--- src/containers/Scraper/Scraper.js
+++ src/containers/Scraper/Scraper.js
@@ -1,64 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 import useSearchQueries from 'store/firebase/hooks/useSearchQueries';
-import { firstCapital } from 'utils';
 import {
 	stripNonChineseChars,
 	stripNonChineseAndNonSetPunctionationChars,
 } from 'utils/chinese';
 import usePrevious from 'hooks/usePrevious';
-import { speak } from 'utils/speech';
 import SearchForm from 'components/SearchForm';
-// import useAnalytics from 'hooks/useAnalytics';
 import CardGrid from 'components/CardGrid/index';
-import translate from 'api/translateApi';
 import TranslationResult from 'components/TranslationResult';
 import Spacer from 'components/Spacer';
-
-// Run once as for some reason the first speak is muted
-speak('');
-
-const splitAndTrim = w => w.split('|').map(s => s.trim());
-
-const getWordListAndSentenceFromTranslation = maskedWord => {
-	const group = splitAndTrim(maskedWord.toLowerCase());
-	const sentence = firstCapital(group[0]);
-	const wordList = group.slice(1);
-	return {
-		sentence,
-		wordList,
-	};
-};
-
-const generateCards = (hanziList, translationList, pinyinList) =>
-	Array(hanziList.length)
-		.fill(0)
-		.map((_, ind) => ({
-			text: hanziList[ind],
-			id: ind + hanziList[ind],
-			hanzi: hanziList[ind],
-			translation: translationList[ind],
-			pinyin: pinyinList[ind],
-		}));
-
-const fetchAndParseTranslationResult = async currentChars => {
-	const hanziGroup = stripNonChineseChars(currentChars).split('');
-	const res = await translate(currentChars);
-	const {
-		sentence: translation,
-		wordList: translationGroup,
-	} = getWordListAndSentenceFromTranslation(res.translation);
-	const {
-		sentence: pinyin,
-		wordList: pinyinGroup,
-	} = getWordListAndSentenceFromTranslation(res.transcription);
-
-	return {
-		translation,
-		pinyin,
-		cards: generateCards(hanziGroup, translationGroup, pinyinGroup),
-	};
-};
+import { fetchAndParseTranslationResult } from './utils';
 
 const Scraper = () => {
 	const [input, setInput] = useState('');
