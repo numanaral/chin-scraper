4df2b047c24d071839283c3aa0eef2f409b9c3a7
numanaral <ahmet.aral@ucalgary.ca>
2020-11-27 05:08:46 -0700

    Updates

--- static/js/5.ad6ca92b.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpchin-205"]=this["webpackJsonpchin-205"]||[]).push([[5],{247:function(t,e,c){"use strict";c.r(e);var s=c(7),n=c(3),i=(c(0),c(173)),a=c(209),r=c(235),u=function(t){return Object(n.jsx)(i.a,{children:Object(n.jsx)(a.a,Object(s.a)({},t))})};u.defaultProps=r.sectionTitleDefaultProps,e.default=u}}]);
