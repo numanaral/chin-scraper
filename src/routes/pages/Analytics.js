52736dd4f7f472206f93892b12f9f4d62867d940
numanaral <ahmet.aral@ucalgary.ca>
2020-11-28 16:04:27 -0700

    Update analytics

--- src/routes/pages/Analytics.js
+++ src/routes/pages/Analytics.js
@@ -1,28 +1,60 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { SuspenseWithPerf } from 'reactfire';
-import { useMediaQuery } from '@material-ui/core';
+import { Avatar, useMediaQuery } from '@material-ui/core';
 
 import LoadingIndicator from 'components/LoadingIndicator';
 import useSearchQueries from 'store/firebase/hooks/useSearchQueries';
 import ContainerWithCenteredItems from 'components/ContainerWithCenteredItems';
 import Toggle from 'components/Toggle';
+import useLocalStorage from 'hooks/useLocalStorage';
 import PageContainer from '../PageContainer';
 
-const getOptions = smAndDown => ({
-	enableTooltip: true,
-	deterministic: false,
-	fontSizes: (smAndDown && [20, 100]) || [50, 200],
-	padding: 1,
-	rotations: 3,
-	rotationAngles: [-15, 15],
-	scale: 'sqrt',
-	spiral: 'archimedean',
-	transitionDuration: 1000,
-});
+const StyledContainerWithCenteredItems = styled(ContainerWithCenteredItems)`
+	height: 95%;
+`;
+
+// Prevents the annoying full-screen size tooltip before the
+// word-cloud loads
+const StyledToggle = styled(Toggle)`
+	${({ theme }) => `
+		max-height: ${theme.spacing(6)}px;
+	`}
+`;
+
+const StyledAvatar = styled(Avatar)`
+	font-weight: 600;
+	${({ theme }) => `
+		width: ${theme.spacing(4)}px;
+    	height: ${theme.spacing(4)}px;
+		font-size: ${theme.spacing(4) / 2.5}px;
+	`}
+`;
+
+const getOptions = (smAndDown, toggle) => {
+	const isCharacter = toggle === 'character';
+	const fontSizes = smAndDown
+		? [20, (isCharacter && 80) || 40]
+		: [50, (isCharacter && 150) || 80];
+	return {
+		enableTooltip: true,
+		deterministic: false,
+		fontSizes,
+		padding: 1,
+		rotations: 3,
+		rotationAngles: [-15, 15],
+		scale: 'sqrt',
+		spiral: 'archimedean',
+		transitionDuration: 1000,
+	};
+};
 
 const normalizeCollection = (collection, type = 'character') => {
 	if (!collection) return [];
-	let _collection = collection.map(c => c.keyword);
+	// TODO: I don't know why but I apparently renamed the key from
+	// "keyword" to "query". Once I fix the keys in firebase, I will
+	// update this
+	let _collection = collection.map(c => c.query || c.keyword);
 	if (type === 'character') {
 		_collection = _collection.join('').split('');
 	}
@@ -36,12 +68,26 @@ const normalizeCollection = (collection, type = 'character') => {
 	return Object.values(_collection);
 };
 
+const getCount = collection => ({
+	words: collection.length,
+	// TODO: I don't know why but I apparently renamed the key from
+	// "keyword" to "query". Once I fix the keys in firebase, I will
+	// update this
+	characters: collection.map(c => c.query || c.keyword).join('').length,
+});
+
+const ANALYTICS_TYPE_LOCAL_STORAGE_KEY = 'analyticsType';
 const Wrapper = () => {
 	const [Component, setComponent] = useState(null);
-	const [toggle, setToggle] = useState('character');
+	const [toggle, setToggle] = useLocalStorage(
+		ANALYTICS_TYPE_LOCAL_STORAGE_KEY,
+		'character'
+	);
 	const [words, setWords] = useState([]);
 	const { collection } = useSearchQueries();
 	const smAndDown = useMediaQuery(theme => theme.breakpoints.down('sm'));
+	const [counts, setCounts] = useState({ words: 0, characters: 0 });
+
 	useEffect(() => {
 		const loadReactWordCloud = async () => {
 			const { default: RWC } = await import('react-wordcloud');
@@ -52,32 +98,42 @@ const Wrapper = () => {
 		};
 		loadReactWordCloud();
 	}, []);
+
 	useEffect(() => {
+		setCounts(getCount(collection));
 		const normalizedCollection = normalizeCollection(collection, toggle);
 		setWords(normalizedCollection);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [toggle]);
 
 	return (
 		<PageContainer>
-			<ContainerWithCenteredItems style={{ height: '95%' }}>
-				<Toggle
+			<StyledContainerWithCenteredItems>
+				<StyledToggle
 					value={toggle}
 					onChange={setToggle}
 					options={[
 						{
 							value: 'character',
-							label: 'By Character',
+							label: 'Characters',
+							icon: (
+								<StyledAvatar>{counts.characters}</StyledAvatar>
+							),
 						},
 						{
 							value: 'word',
-							label: 'By Word',
+							label: 'Words',
+							icon: <StyledAvatar>{counts.words}</StyledAvatar>,
 						},
 					]}
 				/>
 				{((!collection || !Component) && <LoadingIndicator />) || (
-					<Component options={getOptions(smAndDown)} words={words} />
+					<Component
+						options={getOptions(smAndDown, toggle)}
+						words={words}
+					/>
 				)}
-			</ContainerWithCenteredItems>
+			</StyledContainerWithCenteredItems>
 		</PageContainer>
 	);
 };
